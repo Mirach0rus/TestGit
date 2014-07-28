6d5a8a939a33fd1a276fa6e78a778bbd4dc98275
Anton <azh2oon@gmail.com>
2014-07-28 19:08:38 +0700

    First commit

---
+++ testGit/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  testGit
+//
+//  Created by anton on 28.07.14.
+//  Copyright (c) 2014 Altai state university. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ testGit/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  testGit
+//
+//  Created by anton on 28.07.14.
+//  Copyright (c) 2014 Altai state university. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
